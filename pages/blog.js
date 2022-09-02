9463d4a720b9cde9604597f69c8bc010ef130972
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-02 10:34:56 +0200

    check ci with heavy content

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Template from "../templates/Template"
+
 
 const blog = () => {
   return (
-    <Template>
+    <>
 
       <div className="text-8xl" >Blog</div>
-      </Template>
+      </>
   )
 }
 
