a664caf4cc266651658b075935223b989e50b7af
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-05 14:36:11 +0200

    cursor should be small

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,10 +19,12 @@ export default function Example() {
   return (
     <Disclosure as="nav" className="bg-white transition-all duration-400">
       {({ open }) => (
-        <div data-cursor-size="10px"
-        data-cursor-color="#FF595A"
-        data-cursor-exclusion 
-        className="transition-colors duration-400">
+        <div
+          data-cursor-size="10px"
+          data-cursor-color="#FF595A"
+          data-cursor-exclusion
+          className="transition-colors duration-400"
+        >
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -62,7 +64,6 @@ export default function Example() {
                   <div className="flex space-x-10">
                     {navigation.map((item) => (
                       <a
-                        
                         key={item.name}
                         href={item.href}
                         className={classNames(
@@ -79,9 +80,7 @@ export default function Example() {
                   </div>
                 </div>
               </div>
-              <div
-              
-              className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <a
                   href="https://calendly.com/admitas/"
                   className="inline-flex items-center bg-admit-red  border-0 py-2 px-6 focus:outline-none hover:outline transition-b duration-400 hover:bg-admit-blue text-white rounded-full text-sm font-base mt-0 md:mt-0"
