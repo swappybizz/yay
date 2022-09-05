87a709a3be82dc52c46c7f7fdee45699ac83732f
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-05 11:23:48 +0200

    check why data isnt being fetched

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -35,8 +35,9 @@ const poster = [
 
 
 const BlogPreview = ({posts}) => {
+  console.log(posts)
   return (
-    <div div
+    <div
     data-cursor-exclusion style={{backgroundColor: '#fff'}} >
    <section className="text-gray-600 body-font">
    <div className="container px-5 py-24 mx-auto">
@@ -47,7 +48,7 @@ const BlogPreview = ({posts}) => {
        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={post.featuredImage.url} alt={post.title} />
         <div className="p-6">
-          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{poster.category}</h2>
+          {/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">{poster.category}</h2> */}
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{post.title} </h1>
           <p className="leading-relaxed mb-3">{post.excerpt} </p>
           <div className="flex items-center flex-wrap ">
