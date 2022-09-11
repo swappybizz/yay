6d1738f696525019ac2d44dd91d953fc0da13823
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-11 14:56:30 +0200

    push changes

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -35,7 +35,7 @@ const BlogPreview = () => {
                     <div className="flex items-center flex-wrap ">
                       <a
                         href={"/post/" + post.slug}
-                        className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0"
+                        className="text-admit-blue hover:scale-105 hover:text-admit-dark-blue inline-flex items-center md:mb-2 lg:mb-0"
                       >
                         {" "}
                         Les Mer
@@ -53,7 +53,7 @@ const BlogPreview = () => {
                         </svg>
                       </a>
 
-                      <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                      {/* <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                         <svg
                           className="w-4 h-4 mr-1"
                           stroke="currentColor"
@@ -66,7 +66,7 @@ const BlogPreview = () => {
                           <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
                           <circle cx="12" cy="12" r="3"></circle>
                         </svg>
-                        18
+                        
                       </span>
                       <span className="text-gray-400 inline-flex items-center leading-none text-sm">
                         <svg
@@ -81,7 +81,7 @@ const BlogPreview = () => {
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>
                         6
-                      </span>
+                      </span> */}
                     </div>
                   </div>
                 </div>
