ae352be31d4fb459d0bf777160980e54ca574453
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-12 11:23:12 +0200

    commit

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import WeAdmit from "../components/WeAdmit"
 import ThreeColorRings from "../components/ThreeColorRings"
 
 const text1={
-  top: "#WeAdmit",
+  top: "#weadmit",
   middle:"Vår misjon er å skape bærekraftig vekst.",
   bottom:"Bærekraft er et ord som forplikter og viser veg. Gjennom å dele kompetanse kan vi sammen bidra til fremtidsrettet utvikling og bærekraftig vekst, både for organisasjoner og den enkelte arbeidstaker. Nettverk og møteplasser gir større perspektiver, større kompetanse og mer effektive synergier for å nå målene vi har satt oss."
 }
@@ -15,9 +15,9 @@ const text2={
 }
 
 const ringText={
-  leftTop:"8: ANSTENGIG ARBEID OG ØKONOMISK VEKST ",
+  leftTop:"8: ANSTENDIG ARBEID OG ØKONOMISK VEKST",
 leftBottom:"#8 Fremme varig, inkluderende og bærekraftig økonomisk vekst, full sysselsetting og anstendig arbeid for alle. Admit sitt hovedmål er å oppnå varig, inkluderende og bærekraftig økonomisk vekst for egen og andres bedrifter, samt personlig utvikling for den enkelte arbeidstaker. ",
-middleTop:"9: INDUSTRI, INNOVATION OG INFRASTRUCTURE",
+middleTop:"9: INDUSTRI, INNOVATION OG INFRASTRUKTUR",
 middleBottom:"#9 Bygge solid infrastruktur og fremme inkluderende og bærekraftig industrialisering og innovasjon. Gjennom vår Admit Avatar  skaper vi relevante arbeidsplasser i distriktene med klar miljøvennlig virkning. Samtidig får kunden, bedriftene i endring, den kompetansehjelpen de trenger levert digitalt via vår plattform.",
 rightTop:"17: SAMARBEID FOR Å NÅ MÅLENE ",
 rightBottom:"#17 Styrke virkemidlene som trengs for å gjennomføre arbeidet, og fornye globale partnerskap for bærekraftig utvikling. Vårt støtte- og utviklingsmiljø gir grobunn for samskaping og kompetansedeling med mål om fremtidsretta utvikling og bærekraftig vekst."
@@ -25,7 +25,7 @@ rightBottom:"#17 Styrke virkemidlene som trengs for å gjennomføre arbeidet, og
 const about = () => {
   return (
     <>
-      <ThreeMiddleTextTwoButton props={text1} />
+      <ThreeMiddleTextTwoButtonSmall props={text1} />
       <ThreeMiddleTextTwoButtonSmall props={text2} />
       <ThreeColorRings props={ringText}/>
    <WeAdmit/> 
