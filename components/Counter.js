31f473e8fdff2b154ca14300655578f7c8e87d72
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-12 14:47:23 +0200

    pushing changes suggested by monica

--- components/Counter.js
+++ components/Counter.js
@@ -27,7 +27,7 @@ const Counter = () => {
                 </p>
               </div>
               <div className="p-4 sm:w-1/3 w-1/2">
-                <h2 className="title-font font-medium sm:text -8xl font-admitTitle text-3xl text-gray-900">
+                <h2 className="title-font font-medium sm:text-8xl font-admitTitle text-3xl text-gray-900">
                   12
                 </h2>
                 <p className="leading-relaxed font-medium mt-5">
