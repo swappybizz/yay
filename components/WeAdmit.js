d4467875bd685321033a4a6808e61aa4b0c224a4
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-12 15:09:51 +0200

    commit

--- components/WeAdmit.js
+++ components/WeAdmit.js
@@ -4,11 +4,13 @@ const WeAdmit = () => {
   return (
     <section className="text-gray-900 h-screen body-font flex bg-gradient-to-b from-white to-admit-red via-slate-400">
         <div className="container mx-auto flex px-5 items-center justify-center flex-col">
-          <div className="text-center lg:w-2/3 w-full">
+          <div className="text-center flex items-center justify-center lg:w-2/3 w-full">
             {/* <p className="mb-8 leading-relaxed">COWORKING OG MEDLEMSSKAP </p> */}
-            <h1 className="title-font sm:text-8xl font-admitTitle text-6xl mb-4 font-medium text-gray-900">
+            {/* <h1 className="title-font sm:text-8xl font-admitTitle text-6xl mb-4 font-medium text-gray-900">
                     We Admit
-            </h1>
+            </h1> */}
+            <img src="/weadmit.svg" alt="weadmit" className="text-center" />
+
           </div>
         </div>
       </section>
