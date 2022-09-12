3e48bf706f201cfe66dad92cd44389cedad33693
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-12 09:01:01 +0200

    push changes

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home({ posts }) {
       <Circular />
       </div>
       <ThreeMiddleTextTwoButton props={midtext} />
-      {/* <BlogPreview posts={posts} /> */}
+      <BlogPreview posts={posts} />
       <ThreeMiddleTextTwoButton props={tmbttext} />
       <Counter />
       <FullPageImage />
