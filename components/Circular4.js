248300e50a45e51ae41f2745e24970908394c909
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-13 14:29:28 +0200

    circle component

--- components/Circular4.js
+++ components/Circular4.js
@@ -14,14 +14,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/dmYsRWB/4-1-3.png"
+                  src="https://i.ibb.co/x7gbXZH/4-1-5.png"
                 />
                 <div className="px-2 py-2 relative w-48 h-48 p-5 md:w-72 md:h-72 z-10 text-center flex flex-col items-center justify-center  rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     Strategi
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                     For å sette forretningsidéen og visjonen ut i livet må
                     bedriften jobbe frem en overordnet strategi.
                   </p>
@@ -37,14 +37,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/gj8Vhv2/6-1.png"
+                  src="https://i.ibb.co/WVKtj8m/Frame-22.png"
                 />
                 <div className="px-2 py-2 relative z-10 text-center flex flex-col w-48 h-48 p-5 md:w-72 md:h-72 items-center justify-center rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     HR/HMS
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                   For oss i Admit handler HR og HMS om å bygge sterke og velfungerende team, sett i sammenheng med systemtenkning.
                     
                   </p>
@@ -60,14 +60,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/RcQJjMk/5-1.png"
+                  src="https://i.ibb.co/ccLWyjG/5-1-1.png"
                 />
                 <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-48 h-48 p-5 md:w-72 md:h-72 rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Systemtenkning
+                    Systems
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                   Admit jobber med å utvikle lærende organisasjoner gjennom prosessledelse.
                     
                   </p>
@@ -88,14 +88,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/jL7TmnY/Frame-16.png"
+                  src="https://i.ibb.co/Wpmdxsf/Frame-20-1.png"
                 />
                 <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-48 h-48 p-5 md:w-72 md:h-72 rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     Digitalisering
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                   Digitalisering handler om å bruke datamaskiner og annen digital teknologi til å fornye, forenkle og forbedre prosesser.
                   </p>
                 </div>
@@ -110,14 +110,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/DWd7Zpg/Frame-17-1.png"
+                  src="https://i.ibb.co/qkXknTk/Frame-21.png"
                 />
                 <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-48 h-48 p-5 md:w-72 md:h-72 rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     Økonomi
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                     Få bedre oversikt og kontroll over din økonomi og likviditet.
                   </p>
                 </div>
@@ -132,14 +132,14 @@ const Circular4 = () => {
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-48 h-48 p-5 md:w-72 md:h-72 rounded-full object-cover object-center transition-all duration-500"
-                  src="https://i.ibb.co/0cw9SGF/Frame-18-1.png"
+                  src="https://i.ibb.co/kyqXQHj/5-2.png"
                 />
                 <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-48 h-48 p-5 md:w-72 md:h-72 rounded-full border-4 transition-all duration-500 border-admit-blue bg-white opacity-0 hover:opacity-100">
                   {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2> */}
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     Markedsføring
                   </h1>
-                  <p className="leading-relaxed">
+                  <p className="leading-relaxed font-heroSubText">
                     Besøk vår markedsavdeling.
                   </p>
                 </div>
