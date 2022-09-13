bd9b011aab35f2f24d1e0d51cae6b2dfa6edebdc
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-13 11:32:39 +0200

    semifinal circular without main text

--- components/Circular4.js
+++ components/Circular4.js
@@ -9,9 +9,8 @@ const Circular4 = () => {
     className="flex justify-center w-3/4 items-center "
     >
       
-      <a href="/strategy">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/strategy" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/dmYsRWB/4-1-3.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -20,12 +19,10 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
-      <a href="/hr">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/hr" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/gj8Vhv2/6-1.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -34,12 +31,10 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
-      <a href="/systems">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/systems" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/RcQJjMk/5-1.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -48,7 +43,6 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
       
@@ -61,9 +55,8 @@ const Circular4 = () => {
     className="flex justify-center w-3/4 items-center "
     >
       
-      <a href="/digital">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/digital" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/jL7TmnY/Frame-16.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -72,12 +65,10 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
-      <a href="/economy">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/economy" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/DWd7Zpg/Frame-17-1.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -86,12 +77,10 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
-      <a href="/marketing">
-
-      <div className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
+      
+      <a href="/marketing" className="lg:w-1/3 sm:w-1/2 flex justify-center items-center mr-auto">
         <div className="flex relative">
           <img alt="gallery" className="absolute inset-0 w-72 h-72 rounded-full object-cover object-center transition-all duration-400" src="https://i.ibb.co/0cw9SGF/Frame-18-1.png"/>
           <div className="px-2 py-2 relative z-10 text-center flex flex-col items-center justify-center w-72 h-72 rounded-full border-4 transition-all duration-400 border-admit-blue bg-white opacity-0 hover:opacity-100">
@@ -100,7 +89,6 @@ const Circular4 = () => {
             <p className="leading-relaxed">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
           </div>
         </div>
-      </div>
       </a>
       
       
