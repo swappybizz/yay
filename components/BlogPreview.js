cc37b707c0fb80fff3138218cfadcec5b0c93638
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-14 14:39:04 +0200

    hover effect

--- components/BlogPreview.js
+++ components/BlogPreview.js
@@ -15,7 +15,7 @@ const BlogPreview = () => {
      data-cursor-exclusion 
      style={{ backgroundColor: "#fff" }}
     >
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font hover:scale-105 hover:shadow-sm transition-all duration-500">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
             {relatedPosts.map((post) => (
