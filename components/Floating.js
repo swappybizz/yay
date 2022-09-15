4010e542d50c8306e8f2488b6b7499f59d666f4a
Swapnil Kumar <86301852+swappybizz@users.noreply.github.com>
2022-09-15 14:31:49 +0200

    added title tags

--- components/Floating.js
+++ components/Floating.js
@@ -1,39 +1,50 @@
-import React from 'react'
-import { motion,AnimatePresence } from "framer-motion"
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Floating = ({ isVisible }) => {
   return (
-   
     <div className="fabbu">
-    <div className="fab-wrapper">
-  <input id="fabCheckbox" type="checkbox" className="fab-checkbox" />
-  <label className="fab" for="fabCheckbox">
-    <span className="fab-dots fab-dots-1"></span>
-    <span className="fab-dots fab-dots-2"></span>
-    <span className="fab-dots fab-dots-3"></span>
-  </label>
-  <div className="fab-wheel">
-    <a className="fab-action fab-action-1">
-      <i className="fas fa-question">sadsad</i>
-    </a>
-    <a className="fab-action fab-action-2">
-      <i className="fas fa-book"></i>
-    </a>
-        <a className="fab-action fab-action-3">
-      <i className="fas fa-address-book"></i>
-    </a>
-        <a className="fab-action fab-action-4">
-      <i className="fas fa-info"></i>
-    </a>
-  </div>
-</div>
-</div>
+      <div className="fab-wrapper">
+        <input id="fabCheckbox" type="checkbox" className="fab-checkbox" />
+        <label className="fab" for="fabCheckbox">
+          <span className="fab-dots fab-dots-1"></span>
+          <span className="fab-dots fab-dots-2"></span>
+          <span className="fab-dots fab-dots-3"></span>
+        </label>
+        <div className="fab-wheel cursor-pointer">
+          <a
+           title="Avtale møte"
+            href="https://calendly.com/admitas/"
+            className="fab-action fab-action-1 hover:scale-150 flex items-center text-xl justify-center"
+          >
+            <i aria-label="asas" className="fas fa-question  ">
+              📅
+            </i>
+          </a>
+          <a
+            title="Sende E-Post"
+            href="mailto:post@admit.no"
+            className="fab-action fab-action-2 hover:scale-150 text-xl"
+          >
+            <i className="fas fa-book">📧</i>
+          </a>
+          <a
+          title ="snakke på telefon"
+            href="tel:+47 70 32 93 80"
+            className="fab-action fab-action-3 hover:scale-150 flex items-center text-xl justify-center"
+          >
+            <i className="fas fa-address-book">☎️</i>
+          </a>
+          <a 
+          title ="Hjem"
+          
+          href="/" className="fab-action fab-action-4 hover:scale-150 flex items-center text-xl justify-center">
+            <i className="fas fa-info">🏠</i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    
-
-
-
-  )
-}
-
-export default Floating
+export default Floating;
