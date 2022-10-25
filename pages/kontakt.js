d03092f52ccf4e31837e3d56b7df58230a7fbf66
swappybizz <swapnilk0294@gmail.com>
2022-10-25 11:29:57 +0200

    map is modified

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -18,7 +18,7 @@ const contact = () => {
     <>
       <section className="text-gray-600 body-font relative font-heroSubText text-xl">
         <div className="container px-5 py-0 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div className="lg:w-full md:w-full h-[90vh] bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <div className="lg:w-full w-full md:w-full h-[70vh] bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
             <iframe
               width="100%"
               height="100%"
@@ -34,31 +34,9 @@ const contact = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1844.0851721927206!2d6.232233516244679!3d62.47098256228957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4616db47b3f81199%3A0x9b231ca629b20735!2sAdmit%20AS!5e0!3m2!1sen!2sno!4v1663154071526!5m2!1sen!2sno"
               // style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
             ></iframe>
-            <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
-                <h2 className="title-font font-semibold font-admitTitle text-gray-900 tracking-widest text-s">
-                  ADRESSE
-                </h2>
-                <p className="mt-1">
-                Norwegian Maritime Competence Center, Borgundvegen 340, 6009 Ålesund
-                </p>
-              </div>
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                <h2 className="title-font font-semibold font-admitTitle text-gray-900 tracking-widest text-s">
-                  E-POST
-                </h2>
-                <button 
-                onClick={() => window.location = 'mailto:post@admit.com'}
-                className="text-admit-blue leading-relaxed cursor-pointer">
-                  post@admit.no
-                </button>
-                <h2 className="title-font font-semibold font-admitTitle text-gray-900 tracking-widest text-s mt-4">
-                  TELEFON
-                </h2>
-                <p className="leading-relaxed">+47 70 32 93 80</p>
-              </div>
-            </div>
+            
           </div>
+
 
           {/* <form ref={form} onSubmit={sendEmail} className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 className="text-gray-900 text-3xl mb-1 font-admitTitle  font-semibold title-font">
@@ -113,6 +91,37 @@ const contact = () => {
           </form> */}
         </div>
       </section>
+      <div className="bg-white relative flex flex-wrap py-6 md:px-28 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold font-admitTitle text-gray-900 tracking-widest text-s">
+                  ADRESSE
+                </h2>
+                <p className="mt-1">
+                Norwegian Maritime Competence Center, Borgundvegen 340, 6009 Ålesund
+                </p>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0 ">
+                <div className="w-fit animate-pulse rounded ">
+
+                
+                <h2 
+                title="Skrive E-Post"
+                className="title-font  font-semibold font-admitTitle text-gray-900 tracking-widest text-s">
+                  E-POST
+                </h2>
+                <button 
+                title="Skrive E-Post"
+                onClick={() => window.location = 'mailto:post@admit.com'}
+                className="text-admit-blue leading-relaxed cursor-pointer">
+                  post@admit.no
+                </button>
+                </div>
+                <h2 className="title-font font-semibold font-admitTitle text-gray-900 tracking-widest text-s mt-4">
+                  TELEFON
+                </h2>
+                <p className="leading-relaxed">+47 70 32 93 80</p>
+              </div>
+            </div>
     </>
   );
 };
