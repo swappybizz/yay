59004b5939a66fddecd9fa357baaebdcd4d3c7f7
swappybizz <swapnilk0294@gmail.com>
2022-11-18 20:12:33 +0100

    the link tag in next doesnt work in production

--- components/Banner.js
+++ components/Banner.js
@@ -3,11 +3,11 @@ import Link from "next/link"
 
 const Banner = () => {
   return (
-    <Link className="cursor-pointer hidden" href="/camp">
+    <a className="cursor-pointer hidden" href="/camp">
     <div className="flex w-full h-12 bg-admit-red items-center justify-center cursor-pointer">
         <p className="font-medium">Har konkurranseevnen til bedriften din endret seg ?</p>
     </div>
-    </Link>
+    </a>
   );
 };
 
