7a1db3ee51d4514da904964a1ea4bd776f1d20ce
swappybizz <swapnilk0294@gmail.com>
2022-11-21 12:28:40 +0100

    my spellings suck balls

--- components/MainOutput.js
+++ components/MainOutput.js
@@ -23,7 +23,8 @@ const MainOutput = ({ props, gp, pred ,skatt,simuskatt}) => {
       <p className="font-semibold text-l text-admit-red">{pred}</p>
       <div className="w-full flex justify-start" >
           <p className="font-semibold text-sm font-heroSubText text-admit-red">Result for Skatt : {skatt}</p>
-          <p className="font-semibold text-sm font-heroSubText text-admit-red ml-10">2023  simuleret: {simuskatt} </p>
+          <p className="font-semibold text-sm font-heroSubText text-admit-red ml-10">2023  
+simulert: {simuskatt} </p>
 
 
       </div>
