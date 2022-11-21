69519e01528174e1ddf02877b2b80033564a267c
swappybizz <swapnilk0294@gmail.com>
2022-11-21 12:37:27 +0100

    spaces

--- components/MainOutput.js
+++ components/MainOutput.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-
-const MainOutput = ({ props, gp, pred ,skatt,simuskatt}) => {
- 
+const MainOutput = ({ props, gp, pred, skatt, simuskatt }) => {
   return (
     <div className="flex-col bg-[#ededed] border-2 border-gray-400 shadow-lg m-3 ml-6 rounded-xl p-6  max-w-[100%] overflow-hidden pt-4">
       <p className="font-semibold text-xl text-slate-500">Driftsresultat</p>
@@ -21,12 +19,13 @@ const MainOutput = ({ props, gp, pred ,skatt,simuskatt}) => {
       </p>
       <p className="mt-3 font-semibold text-sm text-slate-500">Våre Forslag</p>
       <p className="font-semibold text-l text-admit-red">{pred}</p>
-      <div className="w-full flex justify-start" >
-          <p className="font-semibold text-sm font-heroSubText text-admit-red">Resultat før skatt : {skatt}</p>
-          <p className="font-semibold text-sm font-heroSubText text-admit-red ml-10">2023  
-Simulert: {simuskatt} </p>
-
-
+      <div className="w-full flex justify-start">
+        <p className="font-semibold text-sm font-heroSubText text-admit-red">
+          Resultat før skatt: {skatt}
+        </p>
+        <p className="font-semibold text-sm font-heroSubText text-admit-red ml-10">
+          2023 Simulert: {simuskatt}{" "}
+        </p>
       </div>
     </div>
   );
