bb115dab0853853ebf61b2a8a411048b98ba6060
swapnil kumar <swapnilk0294@gmail.com>
2023-01-23 11:54:08 +0100

    pp

--- pages/marketing.js
+++ pages/marketing.js
@@ -143,7 +143,7 @@ const marketingcamp = () => {
           class=" sm:absolute sm:left-24 md:w-1/3 w-full lg:h-auto h-48 object-cover object-center rounded md:mt-48"
           src="./team.gif"
         />
-        <div class="container px-5  font-admitTitle flex-col md:flex ">
+        <div class="container px-5 md:px-0 md:pr-24  md:ml-auto font-admitTitle flex-col md:flex ">
           <div class="lg:w-1/2 md:w-1/2 bg-transparent rounded-lg p-0 md:p-8 flex flex-col md:ml-auto w-full md:mt-24 relative z-10 shadow-none md:shadow-none">
             <div className="font-heroSubText font-semibold mb-5"></div>
 
@@ -193,7 +193,7 @@ const marketingcamp = () => {
                 </span>
                 <a
                   href="https://outlook.office365.com/owa/calendar/Vidrivermedforretningsutvikling@admit.no/bookings/"
-                  class="flex ml-auto text-white bg-admit-blue border-0 py-2 px-6 focus:outline-none hover:bg-admit-dark-blue rounded"
+                  class="flex ml-auto text-white bg-admit-blue border-0 py-2 px-6 mr-6 focus:outline-none hover:bg-admit-dark-blue rounded"
                 >
                   Book Møte
                 </a>
